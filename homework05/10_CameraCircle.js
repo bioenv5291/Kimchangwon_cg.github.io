15bdba5ba304d35e829eb2d2b0da6cfb1b580d14
bioenv5291 <82022925+bioenv5291@users.noreply.github.com>
2025-04-16 09:17:27 +0900

    Update 10_CameraCircle.js

--- homework05/10_CameraCircle.js
+++ homework05/10_CameraCircle.js
@@ -1,6 +1,6 @@
 import { resizeAspectRatio, setupText, updateText, Axes } from '../util/util.js';
 import { Shader, readShaderFile } from '../util/shader.js';
-import { SquarePyramid } from '../util/squarePyramid.js';
+import { SquarePyramid } from './squarePyramid.js';
 
 const canvas = document.getElementById('glCanvas');
 const gl = canvas.getContext('webgl2');
